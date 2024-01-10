8bd905c5d15929eaaf3f5eef38739a64576fba53
mechy88 <miguelandrerepatacodo@gmail.com>
2024-01-10 21:47:52 +0800

    Update: prototyping for button selection actions

--- script.js
+++ script.js
@@ -1,4 +1,39 @@
-game();
+//DOM selection
+const bato = document.querySelector("#btn_bato");
+const papel = document.querySelector("#btn_papel");
+const guniting = document.querySelector("#btn_gunting");
+const info = document.querySelector("#round_status");
+const score = document.querySelector("#game_score");
+const winner = document.querySelector("#game_winner");
+
+let playerScore = 0;
+let computerScore = 0;
+
+setScore();
+
+
+
+bato.addEventListener('click', function(){
+    info.textContent = round("rock");
+    setScore();
+    win();
+});
+
+papel.addEventListener('click', function(){
+    info.textContent = round("paper");
+    setScore();
+    win();
+});
+
+guniting.addEventListener('click', function(){
+    info.textContent = round("scissors");
+    setScore();
+    win();
+});
+
+function setScore(){
+    score.textContent = `P1: ${playerScore}; P2: ${computerScore}`;
+}
 
 function getComputerChoice(){
     let result = Math.floor((Math.random() * 3));   
@@ -14,6 +49,20 @@ function getComputerChoice(){
     return "IDK";
 }
 
+function win(){
+    if(playerScore == 5){
+        winner.textContent = "YOU WIN!";
+        playerScore = 0;
+        computerScore = 0;
+        setScore();
+    } else if(computerScore == 5){
+        winner.textContent = "YOU LOSE!";
+        playerScore = 0;
+        computerScore = 0;
+        setScore();
+    }
+}
+
 function round(playerSelection){
     playerSelection = playerSelection.toLowerCase();
 
@@ -26,9 +75,11 @@ function round(playerSelection){
     // case user is rock
     if(playerSelection == "rock"){
         if(computer == "paper"){
+            computerScore++;
             return `You Lose! ${computer} beats ${playerSelection}`;
         }
         if(computer == "scissors"){
+            playerScore++;
             return `You Win! ${playerSelection} beats ${computer}`;
         }
     }
@@ -36,9 +87,11 @@ function round(playerSelection){
     // case user is paper
     if(playerSelection == "paper"){
         if(computer == "scissors"){
+            computerScore++;
             return `You Lose! ${computer} beats ${playerSelection}`;
         }
         if(computer == "rock"){
+            playerScore++;
             return `You Win! ${playerSelection} beats ${computer}`;
         }
     }
@@ -46,16 +99,20 @@ function round(playerSelection){
     //case user is scissors
     if(playerSelection == "scissors"){
         if(computer == "rock"){
+            computerScore++;
             return `You Lose! ${computer} beats ${playerSelection}`;
         }
         if(computer == "paper"){
+            playerScore++;
             return `You Win! ${playerSelection} beats ${computer}`;
         }
     }
 }
 
+/*
 function game(){
     for(let i = 0; i < 5; i++){
         alert(round(prompt("Rock, Paper, or Scissors?")));
     }
 }
+*/
